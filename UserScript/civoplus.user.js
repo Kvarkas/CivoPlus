834e8206f6063a6a221f9ca46a1d6e09e0c6def3
Dimitrij <kvarkas@gmail.com>
2018-04-13 15:07:52 +0100

    init

---
+++ UserScript/civoplus.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name       Extend functionality of CIVO gui
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  
+// @match      https://*.civo.com/*
+// @copyright  2018+, Kvark
+// ==/UserScript==
